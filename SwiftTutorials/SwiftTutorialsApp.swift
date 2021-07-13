a1e905d8113ff5cfe5c82c1d343095912a58d5d8
Kodaira Mitsutoshi <kodaira32104@gmail.com>
2021-07-13 14:01:16 +0900

    init

---
+++ SwiftTutorials/SwiftTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTutorialsApp.swift
+//  SwiftTutorials
+//
+//  Created by M K on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
